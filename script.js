00255e7420419eff370cea56ae6c544dc70ccb6a
unknownhatdog <136999477+unknownhatdog@users.noreply.github.com>
2024-01-10 11:36:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -14,10 +14,12 @@ $(document).ready(function () {
     $('#markAsRead').click(function () {
       $('#notifCounter').text(0);
       $('.removeBg').css('backgroundColor', 'white');
-      $('#card').css('display', 'none');
+      $('.card').css('display', 'none');
     })
 
-    $('img').on('click', function () {
-      $(this).toggleClass('viewImage');
+    $('.card').css('display', 'none');
+    $('.cardActive').css('display', 'block');
+    $('img').click(function(){
+      $(this).siblings('.text-xs').find('.card').slideToggle();
     })
   });
